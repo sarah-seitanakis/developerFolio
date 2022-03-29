8dce97d182a9b3db27ffbaeea0e49e6d35ace876
Sarah Seitanakis <sseitanakis@gmail.com>
2022-03-29 17:32:01 -0400

    Updates

---
+++ precache-manifest.326a09c3e23cf45d3679e554ff604fc7.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748ed96b4218136a952edba2a572cf3c",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "4eb87ff6f1fb39432bd9",
+    "url": "/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "c94662e04e2fc10346da",
+    "url": "/developerFolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "60f95dfaa2e46724a3ea",
+    "url": "/developerFolio/static/css/main.7354f574.chunk.css"
+  },
+  {
+    "revision": "f447d7dffe68b21989cd",
+    "url": "/developerFolio/static/js/2.aeff588d.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/developerFolio/static/js/2.aeff588d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb87ff6f1fb39432bd9",
+    "url": "/developerFolio/static/js/3.625aa6e9.chunk.js"
+  },
+  {
+    "revision": "c94662e04e2fc10346da",
+    "url": "/developerFolio/static/js/4.011c95b8.chunk.js"
+  },
+  {
+    "revision": "60f95dfaa2e46724a3ea",
+    "url": "/developerFolio/static/js/main.bae9e9ad.chunk.js"
+  },
+  {
+    "revision": "45fa6910bca51af9893c",
+    "url": "/developerFolio/static/js/runtime-main.da018c69.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "fb8f493d4f628cce673d9be7c96ac3b3",
+    "url": "/developerFolio/static/media/AlleghenyCollegeLogo.fb8f493d.png"
+  },
+  {
+    "revision": "34add73af4743f17494d52368bd5644e",
+    "url": "/developerFolio/static/media/AlleghenyLogo.34add73a.png"
+  },
+  {
+    "revision": "3937699bd995966023001dc363a4c4ac",
+    "url": "/developerFolio/static/media/CoVarLogo.3937699b.png"
+  },
+  {
+    "revision": "9a6b030478b28bce9fff5177371f6146",
+    "url": "/developerFolio/static/media/Hopsify_Logo_thumbnail.9a6b0304.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6e5a9967ff9dc9b9c00b1e8c1fe8d55",
+    "url": "/developerFolio/static/media/Sportivo_logo_thumbnail.d6e5a996.jpg"
+  },
+  {
+    "revision": "fff15c3793e214d40a695fa6731135c6",
+    "url": "/developerFolio/static/media/Teachers_Protect_Logo_thumbnail.fff15c37.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "b9b947ec452ee437f83391544c9c1b25",
+    "url": "/developerFolio/static/media/contactMailFemale.b9b947ec.svg"
+  },
+  {
+    "revision": "8340b0f89b7f63a114e5ec4592e01cfd",
+    "url": "/developerFolio/static/media/devshoplogo.8340b0f8.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "d95a15dab5fd1b79c7a69a914e8514fe",
+    "url": "/developerFolio/static/media/starsmasherFigure.d95a15da.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "7438b884da9b1f6c6df189b83abf2205",
+    "url": "/developerFolio/static/media/undraw_creative_woman_re_u5tk.7438b884.svg"
+  },
+  {
+    "revision": "3d85b17ea21f0e1f34f1455f0a59a855",
+    "url": "/developerFolio/static/media/undraw_designer_life_re_6ywf.3d85b17e.svg"
+  },
+  {
+    "revision": "84e4319d3a2152a5384e827509abc07f",
+    "url": "/developerFolio/static/media/undraw_dev_focus_re_6iwt.84e4319d.svg"
+  }
+]);
